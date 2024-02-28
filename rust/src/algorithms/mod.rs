27f1749d76655fb9fe343c83ed221d5d41ffad33
lee <leeian1011@gmail.com>
2024-02-28 22:51:20 +0800

    feat: quick sort

--- rust/src/algorithms/mod.rs
+++ rust/src/algorithms/mod.rs
@@ -1,3 +1,4 @@
 pub mod binary_search;
+pub mod quick_sort;
 pub mod recursive;
 pub mod selection_sort;
---
+++ rust/src/algorithms/quick_sort.rs
@@ -0,0 +1,36 @@
+// This qsort takes it's pivot from the end
+pub fn qsort(arr: &mut [u8]) {
+    let length = arr.len();
+    if length < 2 {
+        return;
+    }
+
+    if length == 2 {
+        if arr[0] > arr[1] {
+            let temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+
+        return;
+    }
+
+    let pivot = arr[length - 1];
+    let mut actual_index = 0;
+
+    for i in 0..(length - 1) {
+        if arr[i] <= pivot {
+            let temp = arr[i];
+            arr[i] = arr[actual_index];
+            arr[actual_index] = temp;
+            actual_index += 1;
+        }
+    }
+
+    let temp = arr[actual_index];
+    arr[actual_index] = arr[length - 1];
+    arr[length - 1] = temp;
+
+    qsort(&mut arr[..actual_index]);
+    qsort(&mut arr[actual_index..]);
+}
